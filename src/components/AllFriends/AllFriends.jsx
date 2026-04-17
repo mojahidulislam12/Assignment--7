064f733cac94e4d7e8067c62b9c7374790255ae6
Sagor <mojahidulislamsagor1234@gmail.com>
2026-04-17 10:01:47 +0600

    AllFriend Section add Suspens

--- src/components/AllFriends/AllFriends.jsx
+++ src/components/AllFriends/AllFriends.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React, { Suspense, use } from "react";
 import FriendCard from "../FriendCard/FriendCard";
 
 const fiendsPromise = fetch("/Friends.json").then((res) => res.json());
@@ -9,9 +9,15 @@ const AllFriends = () => {
     <div className="max-w-277.5 mx-auto mt-10">
       <h1 className="font-semibold text-[24px]">Your Friends</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-5 lg:gap-6 mt-4 pb-20">
-        {friends.map((friend, index) => (
-          <FriendCard friend={friend} key={index}></FriendCard>
-        ))}
+        <Suspense
+          fallback={
+            <span className="loading loading-spinner loading-xl"></span>
+          }
+        >
+          {friends.map((friend, index) => (
+            <FriendCard friend={friend} key={index}></FriendCard>
+          ))}
+        </Suspense>
       </div>
     </div>
   );
